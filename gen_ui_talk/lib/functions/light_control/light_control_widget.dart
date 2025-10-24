ee3035026e268dd892d7907f52b46b4338e50730
Leo Farias <leoafarias@gmail.com>
2025-10-24 17:16:35 -0400

    Moved directory structure

---
+++ gen_ui_talk/lib/functions/light_control/light_control_widget.dart
@@ -0,0 +1,378 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import '../../ai/components/molecules/llm_content_view.dart';
+import '../../ai/controllers/chat_controller.dart';
+import '../../ai/models/llm_response.dart';
+import '../../ai/style.dart';
+import 'light_control_controller.dart';
+import 'light_control_dto.dart';
+
+class _LightControlWidgetResponse extends HookWidget {
+  const _LightControlWidgetResponse(this.data);
+
+  final LightControlDto data;
+
+  void _updateBrightness(num value) {
+    lightControlController.setBrightness(value.toInt());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final brightness = useState(data.brightness);
+    final previousBrightness = usePrevious(brightness.value);
+
+    final chatController = ChatController.of(context);
+
+    void handleConfirmation() {
+      var message = 'Confirmed';
+      if (brightness.value != data.brightness) {
+        if (brightness.value == 0.0) {
+          message = 'Light turned off';
+        } else {
+          message = 'Light level updated to ${brightness.value}%';
+        }
+      }
+
+      chatController.addSystemMessage(message);
+    }
+
+    final isOn = brightness.value != 0;
+
+    Widget buildDisplay() {
+      return Row(
+        children: [
+          Icon(
+            Icons.light_mode_outlined,
+            size: 30,
+            color: chatTheme.backgroundColor,
+          ),
+          Expanded(
+            child: SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                activeTrackColor: chatTheme.backgroundColor,
+                inactiveTrackColor: chatTheme.backgroundColor.withOpacity(0.5),
+                trackHeight: 8.0,
+                thumbColor: chatTheme.backgroundColor,
+                overlayShape: SliderComponentShape.noOverlay,
+                activeTickMarkColor: Colors.white,
+                inactiveTickMarkColor: Colors.grey,
+              ),
+              child: Slider(
+                min: 0,
+                max: 100,
+                value: brightness.value.toDouble(),
+                onChangeEnd: _updateBrightness,
+                onChanged: (value) => brightness.value = value.toInt(),
+              ),
+            ),
+          ),
+          Icon(
+            Icons.light_mode,
+            size: 30,
+            color: chatTheme.backgroundColor,
+          ),
+        ],
+      );
+    }
+
+    return Row(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Container(
+            padding: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              color: chatTheme.onBackGroundColor,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Lights",
+                              style: chatTheme.textStyle.copyWith(
+                                fontSize: 22,
+                                color: chatTheme.backgroundColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 4.0),
+                              constraints: const BoxConstraints(minWidth: 45),
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: chatTheme.backgroundColor,
+                                borderRadius: BorderRadius.circular(5.0),
+                              ),
+                              child: Text(isOn ? '${brightness.value}%' : 'Off',
+                                  style: chatTheme.textStyle.copyWith(
+                                    color: chatTheme.accentColor,
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ),
+                      buildDisplay(),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: RotatedBox(
+                    quarterTurns: 3,
+                    child: LightSwitch(
+                      value: isOn,
+                      onChanged: (value) {
+                        final previousValue = previousBrightness == null ||
+                                previousBrightness == 0
+                            ? 100
+                            : previousBrightness;
+
+                        brightness.value = value ? previousValue : 0;
+                        _updateBrightness(brightness.value.toDouble());
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 10.0),
+          decoration: BoxDecoration(
+            color: chatTheme.accentColor,
+            shape: BoxShape.circle,
+          ),
+          padding: const EdgeInsets.all(8.0),
+          child: IconButton(
+            color: chatTheme.onAccentColor,
+            onPressed: handleConfirmation,
+            icon: const Icon(Icons.check),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class LightControlWidgetResponse extends HookWidget {
+  final LlmFunctionElement element;
+  const LightControlWidgetResponse(this.element, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = LlmContentViewProvider.of(context);
+    if (!provider.active) {
+      return const SizedBox.shrink();
+    }
+
+    if (!element.isComplete) {
+      return const _LightControlWidgetSkeleton();
+    }
+    return _LightControlWidgetResponse(
+        LightControlDto.fromMap(element.requiredData));
+  }
+}
+
+class LightSwitch extends StatefulWidget {
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  const LightSwitch({
+    super.key,
+    required this.value,
+    required this.onChanged,
+  });
+
+  @override
+  _LightSwitchState createState() => _LightSwitchState();
+}
+
+class _LightSwitchState extends State<LightSwitch>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+  late Animation<Color?> colorAnimation;
+  late Animation<Color?> thumbColorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+
+    animation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeInOut,
+    );
+
+    colorAnimation = ColorTween(
+      begin: Colors.grey[700],
+      end: chatTheme.backgroundColor,
+    ).animate(animation);
+
+    thumbColorAnimation = ColorTween(
+      begin: Colors.grey[300],
+      end: chatTheme.onBackGroundColor,
+    ).animate(animation);
+
+    // Set the initial animation state based on the initial value
+    if (widget.value) {
+      controller.value = 1.0;
+    } else {
+      controller.value = 0.0;
+    }
+  }
+
+  @override
+  void didUpdateWidget(LightSwitch oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // Update the animation when the value changes externally
+    if (oldWidget.value != widget.value) {
+      if (widget.value) {
+        controller.forward();
+      } else {
+        controller.reverse();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void toggleSwitch() {
+    widget.onChanged(!widget.value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const switchWidth = 100.0;
+    const switchHeight = 50.0;
+    const borderRadius = switchHeight / 2;
+
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: toggleSwitch,
+        child: AnimatedBuilder(
+          animation: controller,
+          builder: (context, child) {
+            return Container(
+              width: switchWidth,
+              height: switchHeight,
+              decoration: BoxDecoration(
+                color: colorAnimation.value,
+                borderRadius: BorderRadius.circular(borderRadius),
+              ),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Positioned(
+                    left: 10 + (switchWidth - switchHeight) * animation.value,
+                    child: Container(
+                      width: switchHeight - 20,
+                      height: switchHeight - 20,
+                      decoration: BoxDecoration(
+                        color: thumbColorAnimation.value,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _LightControlWidgetSkeleton extends StatefulWidget {
+  const _LightControlWidgetSkeleton();
+
+  @override
+  _LightControlWidgetSkeletonState createState() =>
+      _LightControlWidgetSkeletonState();
+}
+
+class _LightControlWidgetSkeletonState
+    extends State<_LightControlWidgetSkeleton> {
+  double _opacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 20), () {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedOpacity(
+      duration: const Duration(milliseconds: 500),
+      opacity: _opacity,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              constraints: const BoxConstraints(minHeight: 125),
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.white.withOpacity(0.2),
+                    Colors.white.withOpacity(0.4),
+                    Colors.white.withOpacity(0.2),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                colors: [
+                  Colors.white.withOpacity(0.2),
+                  Colors.white.withOpacity(0.4),
+                  Colors.white.withOpacity(0.2),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ gen_ui_talk/slides.md
@@ -0,0 +1,561 @@
+---
+
+@section {
+  flex: 2
+}
+@column {
+  align: center
+}
+# Generative UI {.heading}
+# with Flutter {.subheading}
+
+---
+
+@column {
+  align: center
+}
+
+#### Leo Farias {.heading}
+#### @leoafarias {.subheading}
+
+@column {
+  align: center_left
+}
+- Founder/CEO/CTO
+- Open Source Contributor (fvm, mix, superdeck, others..)
+- Flutter & Dart GDE
+- Passionate about UI/UX/DX
+
+
+---
+
+@column
+
+@column {
+  align: center_left 
+  flex: 2
+}
+> [!WARNING]  
+> This presentation contains live AI-generated content. Unexpected things may occur during the demonstration.
+
+@column
+
+---
+
+@column {
+  flex: 2 
+  align: center_right
+}
+### Generative UI {.heading}
+@column
+## VS 
+@column {
+  flex: 2
+}
+### AI Assisted Code Generation
+
+<!-- Code Generation code as the output to boost developer productivity vs Generates widgets definitions to be rendered on the screen -->
+
+---
+
+### What is Generative UI? {.heading}
+
+@column
+
+- LLMs are great at generating content based on context
+- GUIs are great at providing structured, interactive interfaces for user input and navigation
+
+
+---
+
+# LLM ❤️ GUI {.heading}
+
+---
+
+@column
+@column {
+  flex: 5
+  align: center
+}
+### Creates dynamic, context-aware UIs by interpreting actions and maintaining state with LLMs for fluid, interactive responses. {.heading}
+
+@column
+
+---
+
+### Benefits over Chat {.heading}
+
+- More intuitive and user-friendly, especially for complex tasks
+- Faster feedback loop between users and LLMs
+- Enhances efficiency and interaction
+
+---
+
+@column
+
+@column {
+  flex: 3 
+  align: center
+}
+### Flutter is Well-Suited for Generative UI {.heading}
+Built for any screen: Ideal for generating adaptive UIs across devices and platforms.
+
+@column
+
+---
+
+## Generative UI workflow
+
+@section
+
+@column {
+  align: top_center
+}
+```mermaid
+flowchart LR
+    A[Input] --> B[LLM]
+    B --> C[Structured Response]
+    C --> D[UI]
+```
+
+---
+
+### How can LLMs Understand Your UI? {.heading}
+
+@column
+
+![structured_output](assets/structured_output.png)
+
+
+---
+
+@column
+
+### Define the Schema {.heading}
+
+@column
+
+```dart
+final schema = Schema.array(
+  description: 'List of recipes',
+  items: Schema.object(
+    properties: {
+      'recipeName': Schema.string(
+        description: 'Name of the recipe.',
+        nullable: false,
+      ),
+    },
+    requiredProperties: ['recipeName'],
+  ),
+);
+
+```{.code}
+
+---
+
+@column {
+  align: bottom_center
+}
+
+### Configure responseMimeType & responseSchema {.heading}
+
+@column
+
+```dart
+final schema = Schema.array(
+  description: 'List of recipes',
+  items: Schema.object(
+    properties: {
+      'recipeName': Schema.string(
+        description: 'Name of the recipe.',
+        nullable: false,
+      ),
+    },
+    requiredProperties: ['recipeName'],
+  ),
+);
+
+
+final model = GenerativeModel(
+  model: 'gemini-1.5-pro',
+  apiKey: apiKey,
+  generationConfig: GenerationConfig(
+    responseMimeType: 'application/json',
+    responseSchema: schema,
+  ),
+);
+
+```{.code}
+
+---
+
+@section {
+  flex: 2
+}
+
+@column {
+  flex: 2
+}
+
+#### Passing a prompt to your model {.heading}
+
+@column {
+  flex: 3
+}
+
+```dart
+final prompt = 'List a few popular cookie recipes.';
+final response = await model.generateContent([Content.text(prompt)]);
+
+print(response.candidates.first.content);
+
+```{.code}
+
+@section {
+  flex: 3
+}
+
+@column {
+  flex: 2
+  align: bottom_left
+}
+
+#### JSON Response
+
+@column {
+  flex: 3
+}
+
+```json
+[
+  {
+    "recipeName": "Chocolate Chip Cookies"
+  },
+  {
+    "recipeName": "Peanut Butter Cookies"
+  },
+  {
+    "recipeName": "Snickerdoodles"
+  }
+]
+```
+
+
+
+---
+
+@column
+
+### Color Palette Generator
+
+Generate a color palette based on a given text.
+
+- Name of the palette
+- Font family
+- Font color
+- Color for each corner of the palette
+
+--- 
+
+@column{
+
+}
+
+## Color Palette Schema
+
+@column{
+  scrollable: true
+  flex: 2
+}
+
+```dart
+final schema = Schema.object(
+  properties: {
+  'name': Schema.string(
+    description:'Name of the color palette',
+    nullable: false,
+  ),
+  'font': Schema.enumString(
+    enumValues: ColorPaletteFontFamily.enumString,
+    description: 'Font to use for color palette name',
+    nullable: false,
+  ),
+  'fontColor': Schema.string(
+    description: 'Font color. Format: #FF0000',
+    nullable: false,
+  ),
+  'topLeftColor': Schema.string(
+    description: 'Top left color. Format: #FF0000',
+    nullable: false,
+  ),
+  'topRightColor': Schema.string(
+    description: 'Top right color. Format: #FF0000',
+    nullable: false,
+  ),
+  'bottomLeftColor': Schema.string(
+    description: 'Bottom left color. Format: #FF0000',
+    nullable: false,
+  ),
+  'bottomRightColor': Schema.string(
+    description: 'Bottom right color. Format: #FF0000',
+    nullable: false,
+  )
+}, requiredProperties: [
+  'name',
+  'font',
+  'fontColor',
+  'topLeftColor',
+  'topRightColor',
+  'bottomLeftColor',
+    'bottomRightColor',
+  ],
+);
+
+```
+
+---
+style: demo
+---
+
+@colorPalette {
+  kind: schema
+  prompts:
+    - tropical
+    - vibrant
+    - pastel
+    - chocolatey pink unicorn
+    - cyberpunk
+}
+
+---
+style: demo
+---
+
+@colorPalette {
+  kind: widget
+  prompts:
+    - tropical
+    - vibrant
+    - pastel
+    - chocolatey pink unicorn
+    - cyberpunk
+}
+
+---
+
+## LLMs Orchestrate APIs
+
+---
+
+@section
+@column
+#### Gemini Function Calling
+@column
+The Function Calling feature is in Beta release
+
+@section {
+  flex: 4
+}
+![llm tools](assets/llm_tools.png){.cover}
+
+
+
+---
+style: demo
+---
+
+@lightControl {
+  kind: schema
+  prompts:
+    - Dim the lights by 20
+    - Increase by 35
+    - Turn off the lights
+    - Set it to 80
+    - Lower by half
+    - Max brightness
+}
+
+--- 
+
+@column {
+  align: bottom_right
+}
+
+### User Interaction {.heading}
+Natural Language way to interact with an LLM
+
+@column {
+  flex: 2
+}
+![llm response](assets/llm_interaction.png)
+
+
+---
+
+
+@column {
+  align: center_left
+}
+### Widget Response {.heading}
+
+@column {
+  flex: 3
+}
+![widget_response](assets/widget_response.png)
+
+
+---
+style: demo
+---
+
+@lightControl {
+  kind: widget
+  prompts:
+    - Dim the lights by 20
+    - Increase by 35
+    - Turn off the lights
+    - Set it to 80
+    - Lower by half
+    - Max brightness
+}
+
+---
+
+## What if the tool to use is a widget schema?
+
+---
+
+![widget_schema](assets/widget_schema.png)
+
+---
+
+@column {
+  flex: 2
+}
+### Dropdown Widget Schema
+
+```dart
+class DropdownSchemaDto {
+  final String label;
+  final String currentValue;
+  final List<String> options;
+
+  DropdownSchemaDto({
+    required this.label,
+    required this.currentValue,
+    required this.options,
+  });
+
+```
+@column {
+  flex: 3
+}
+
+```dart
+  static final schema = Schema.object(properties: {
+    'label': Schema.string(
+      description: 'The label of the dropdown',
+      nullable: false,
+    ),
+    'currentValue': Schema.string(
+      description: 'The currentValue of the dropdown',
+      nullable: false,
+    ),
+    'options': Schema.array(
+      description: 'The options of the dropdown',
+      items: Schema.string(
+        description: 'The options of the dropdown',
+        nullable: false,
+      ),
+      nullable: false,
+    )
+  });
+}
+```
+
+---
+
+### Dropdown Schema Builder
+
+@column {
+  flex: 2
+}
+```dart
+ ListTile(
+  title: Text(schema.label),
+  trailing: DropdownButton<String>(
+    value: schema.currentValue,
+    items: schema.options.map((option) {
+      return DropdownMenuItem<String>(
+        value: option,
+        child: Text(option),
+      );
+    }).toList(),
+    onChanged: (newValue) {},
+  ),
+);
+```
+
+---
+
+### Widget Schemas
+
+
+```dart
+final schema = Schema.object(properties: {
+  'textFields': Schema.array(
+    description: 'A list of text fields',
+    items: TextFieldSchemaDto.schema,
+    nullable: true,
+  ),
+  'dropdowns': Schema.array(
+    description: 'A list of dropdowns',
+    items: DropdownSchemaDto.schema,
+    nullable: true,
+  ),
+  'colorPickers': Schema.array(
+    description: 'A list of colors pickers',
+    items: ColorPickerDtoSchema.schema,
+    nullable: true,
+  ),
+});
+
+```
+
+---
+style: demo
+---
+
+@widgetSchema {
+  kind: widget
+  prompts:
+    - Change top colors
+    - Change font
+    - Change all colors
+    - Change everything
+}
+
+---
+style: quote
+---
+
+> The future of UI might be orchestrating
+> user experiences rather than creating it.
+
+---
+
+@section{
+  align: bottom_center
+  flex: 2
+}
+# Thank you
+
+@section
+Leo Farias
+_@leoafarias
+(GitHub, Twitter/X)
+
+@column 
+
+#### Slides + Code
+https://github.com/leoafarias/gen-ui-talk
